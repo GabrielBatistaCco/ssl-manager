6b5a7ecca243c5d0a1c99e65b4398a604ff26287
Gabriel de Oliveira <gabriel7251302@gmail.com>
2023-11-16 16:48:24 -0300

    Validação para cadastro de domínios duplicados adicionada.

--- python/app_ssl/models.py
+++ python/app_ssl/models.py
@@ -11,23 +11,49 @@ class StatusSSL(models.TextChoices):
 
 class Cert (models.Model):
     id = models.AutoField(primary_key=True)
-    dominio = models.CharField(verbose_name="Domínio", max_length=100)
-    url_ssls = models.CharField(verbose_name="URL ssls", null=True, max_length=100)
-    validade_ssl = models.DateField(verbose_name="Validade certificado", null=True)
-    issuer = models.CharField(verbose_name="Emissor", null=True, max_length=100)
+    dominio = models.CharField(
+        verbose_name="Domínio", 
+        null=False, 
+        unique=True, 
+        error_messages={
+            'unique': 'Este domínio já está cadastrado.',
+        },
+        max_length=100
+    )
+    url_ssls = models.CharField(
+        verbose_name="URL ssls", 
+        null=True, 
+        max_length=100
+    )
+    validade_ssl = models.DateField(
+        verbose_name="Validade certificado", 
+        null=True
+    )
+    issuer = models.CharField(
+        verbose_name="Emissor", 
+        null=True, 
+        max_length=100
+    )
     status_ssl = models.CharField(
         max_length=10,
         choices=StatusSSL.choices,
         verbose_name='Status SSL',
         null=True,
     )
-    criado_em = models.DateTimeField(verbose_name="Data cadastro", auto_now_add=True, null=False, blank=False)
-
+    criado_em = models.DateTimeField(
+        verbose_name="Data cadastro", 
+        auto_now_add=True, 
+        null=False, 
+        blank=False
+    )
 
     def clean(self):
         super().clean()
 
         if self.url_ssls:
+            if Cert.objects.filter(dominio=self.dominio).exclude(pk=self.pk).exists():
+                raise ValidationError({'dominio': self.field.error_messages['unique']})
+
             if not (self.url_ssls.startswith('http://') or self.url_ssls.startswith('https://')):
                 self.url_ssls = f'https://{self.url_ssls}'
 
